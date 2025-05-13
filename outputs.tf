68610429aa644613c4809c7d6087e433a03adf8c
Dheimy <jheimyeng@hotmail.com>
2025-05-13 00:49:13 -0300

    fix: output.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,24 @@
-output "arn" {
-  value = aws_lambda_function.lambda_function.arn
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.lambda.arn
 }
 
-output "function_name" {
-  value = aws_lambda_function.lambda_function.function_name
+output "lambda_function_name" {
+  description = "The name of the Lambda function"
+  value       = aws_lambda_function.lambda.function_name
 }
+
+output "lambda_function_invoke_arn" {
+  description = "The ARN to be used for invoking Lambda Function from API Gateway"
+  value       = aws_lambda_function.lambda.invoke_arn
+}
+
+output "lambda_role_arn" {
+  description = "The ARN of the IAM role created for the Lambda function"
+  value       = aws_iam_role.lambda_role.arn
+}
+
+output "lambda_role_name" {
+  description = "The name of the IAM role created for the Lambda function"
+  value       = aws_iam_role.lambda_role.name
+} 
